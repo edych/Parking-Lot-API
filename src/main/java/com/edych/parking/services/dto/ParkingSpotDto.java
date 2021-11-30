9aeaefa59fef8d40353a9f52d15355a61411367d
edych <edytapatrycja.chrzanowska@gmail.com>
2021-11-30 15:54:42 +0100

    added dto's. closes #31

---
+++ src/main/java/com/edych/parking/services/dto/ParkingSpotDto.java
@@ -0,0 +1,18 @@
+package com.edych.parking.services.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParkingSpotDto {
+
+    private Long id;
+    private Integer number;
+    private Integer floor;
+    private Boolean handicapped;
+}
